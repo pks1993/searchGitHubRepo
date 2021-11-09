fad86b41a168f7a16e1886291268717883d97125
phyokyawswar.1993 <phyokyawswar.1993@gmail.com>
2021-11-09 09:09:48 +0630

    First commit

---
+++ searchGitHubRepo/searchGitHubRepo/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  searchGitHubRepo
+//
+//  Created by Phyo Kyaw Swar on 09/11/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+   
+
+}
+
